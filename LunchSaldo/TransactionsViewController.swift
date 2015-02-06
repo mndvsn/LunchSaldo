0beeccbdd06e822b56a61badd217306625fb3b37
Martin Furuberg <martin.furuberg@factory.fb.se>
2015-02-06 12:22:40 +0100

    fix runtime bug with transaction cell

--- LunchSaldo/TransactionsViewController.swift
+++ LunchSaldo/TransactionsViewController.swift
@@ -28,13 +28,13 @@ struct Transaction: Printable {
   }
 }
 
+class TransactionCell: UITableViewCell {
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var amountLabel: UILabel!
+}
+
 class TransactionsViewController: UITableViewController {
 
-  class TransactionCell: UITableViewCell {
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var amountLabel: UILabel!
-  }
-  
   @IBOutlet weak var loadMoreButton: UIBarButtonItem!
   
   let cardId = NSUserDefaults.standardUserDefaults().integerForKey(AppSettings.Key.RikslunchenCardID.rawValue)
